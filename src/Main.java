182ec6c1bed98d3d61cc6a77352e73eea8d67ee0
Vasilii Ostrovskii <Nezero@mail.ru>
2023-04-05 14:29:19 +0900

    Домашка методы Выполнено и дополнено

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,29 @@ public class Main {
         else
             System.out.println(year+" год не является високосным");
     }
-    public static void clientOsYear(int clientDiviceYear) {
+    public static void clientOsYear(int clientDiviceYear,int clientOS) {
 
         if (clientDiviceYear < 2023) {
             System.out.println("Установите ограниченную версию приложения ");
         } else if (clientDiviceYear >= 2023) {
             System.out.println("Установите версию приложения ");
-        }    }
-    public static void clientOs(int clientOS) {
+        }
         if (clientOS == 0) {
             System.out.println("для IOS по ссылке");
         }
         if (clientOS == 1) System.out.println("для Android по ссылке");
     }
-    public static void diliveryDistance(int diliveryDistsnce){
+    public static int diliveryDistanceCl (int diliveryDistsnce){
         if (diliveryDistsnce<=20)
             System.out.println("Потребуется 1 день");
         if (diliveryDistsnce>20&&diliveryDistsnce<60)
             System.out.println("Потребуется 2 дня");
         else if (diliveryDistsnce>60&&diliveryDistsnce<100)
             System.out.println("Потребуется 3 дня");
+        else if (diliveryDistsnce>100)
+            System.out.println("доставки нет");
+        return  diliveryDistsnce;}
 
-        else System.out.println("доставки нет");}
 
 
     public static void main(String[] args) {
@@ -44,17 +45,15 @@ public class Main {
     public static void task2() { System.out.println("Задача №2");
 
         int currentYear = LocalDate.now().getYear();
-        clientOsYear(currentYear);
-        int clientOS = 0;
-        clientOs(clientOS);
-
-
+        int clientOS = 1;
+        clientOsYear(currentYear,clientOS);
     }
 
     public static void task3() {
         System.out.println("Задача №3");
-        int diliveryDistsnce=100;
-        diliveryDistance(diliveryDistsnce);
+        int diliveryDistsnce=101;
+        diliveryDistanceCl(diliveryDistsnce);
+
 
     }
 }
